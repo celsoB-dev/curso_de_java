37d029a8e3a467d86db219e434e39e98ce51df5d
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-03-15 17:28:05 -0300

    aula de membros estáticos

---
+++ pacote_download/Curso de Java/aulas-de-poo/membros-estaticos-parte2/src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Calculator;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius: ");
+        double radius = scanner.nextDouble();
+
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+        scanner.close();
+    }
+}
