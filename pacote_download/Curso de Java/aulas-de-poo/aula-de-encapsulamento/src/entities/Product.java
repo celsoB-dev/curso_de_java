d3fa22356a78f632e76edef12b50408c609c2968
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-03-15 21:44:42 -0300

    Update Product.java

--- pacote_download/Curso de Java/aulas-de-poo/aula-de-encapsulamento/src/entities/Product.java
+++ pacote_download/Curso de Java/aulas-de-poo/aula-de-encapsulamento/src/entities/Product.java
@@ -9,10 +9,10 @@ public class Product {
     public Product() {
     }
 
-    public Product(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
+    public Product(int quantity, double price, String name) {
         this.quantity = quantity;
+        this.price = price;
+        this.name = name;
     }
 
     public Product(String name, double price) {
