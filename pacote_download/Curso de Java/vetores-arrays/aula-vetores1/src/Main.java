f72301fc35b06b4af692a9a505f5263e98659bfb
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-06 15:39:23 -0300

    aula vetores

---
+++ pacote_download/Curso de Java/vetores-arrays/aula-vetores1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+        scanner.close();
+    }
+}
