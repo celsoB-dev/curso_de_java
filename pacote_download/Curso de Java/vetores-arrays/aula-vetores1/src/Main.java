522b9c0b870fd99a8f57775e19a59c650cdc5702
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-06 16:04:23 -0300

    Update Main.java

--- pacote_download/Curso de Java/vetores-arrays/aula-vetores1/src/Main.java
+++ pacote_download/Curso de Java/vetores-arrays/aula-vetores1/src/Main.java
@@ -4,10 +4,24 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args){
 
+        //Vetor com elementos do tipo valor
+
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        
+        int n = scanner.nextInt();
+        double[] vect = new double[n];
+        double sum = 0;
+
+        for(int i = 0; i < n; i++){
+            vect[i] = scanner.nextDouble();
+        }
+        for(int j = 0; j < n; j++){
+            sum += vect[j];
+        }
+        double media = sum /n;
+
+        System.out.printf("%nAverage height %.2f%n", media);
 
         scanner.close();
     }
