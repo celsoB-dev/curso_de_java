2d5354ba9ce0b688a21f00414606d522bbc9ba65
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-06 17:08:45 -0300

    aula-vetores2

---
+++ pacote_download/Curso de Java/vetores-arrays/aula-vetores2/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        Product[] vect = new Product[n];
+
+        for(int i = 0; i < n; i++){
+            scanner.nextLine();
+            String name = scanner.nextLine();
+            double price = scanner.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double sum = 0.0;
+        for(int i = 0; i < n; i++){
+            sum += vect[i].getPrice();
+        }
+
+        double avg = sum / n;
+
+        System.out.printf("%nAverage price = $%.2f%n", avg);
+
+        scanner.close();
+    }
+}
