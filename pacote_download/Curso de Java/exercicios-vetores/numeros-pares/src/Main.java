f1aed7941b1e7bd25d5bd4f21f46ae636bdeaaf7
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-19 11:37:42 -0300

    Conclusão do exercício

--- pacote_download/Curso de Java/exercicios-vetores/numeros-pares/src/Main.java
+++ pacote_download/Curso de Java/exercicios-vetores/numeros-pares/src/Main.java
@@ -8,12 +8,35 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int contadorPar = 0;
+
         System.out.print("Quantos numeros voce vai digitar? ");
         int n = scanner.nextInt();
 
         int[] vetor = new int[n];
 
-        
+        for(int i = 0; i < n; i++){
+            System.out.print("Digite um número: ");
+            vetor[i] = scanner.nextInt();
+
+            if(vetor[i] % 2 == 0){
+                contadorPar++;
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("NÚMEROS PARES: ");
+
+        for(int i = 0; i < n; i++){
+            if(vetor[i] %  2 ==0){
+                System.out.print(vetor[i] + " ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("QUANTIDADE DE PARES: " + contadorPar);
 
         scanner.close();
     }
