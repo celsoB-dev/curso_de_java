0644ec7759816a439edfeedbcfd8de0f9d7252ea
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-18 21:20:26 -0300

    Mudanças

--- pacote_download/Curso de Java/exercicios-vetores/alturas/src/application/Main.java
+++ pacote_download/Curso de Java/exercicios-vetores/alturas/src/application/Main.java
@@ -1,7 +1,5 @@
 package application;
 
-import entities.Pessoas;
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -15,12 +13,42 @@ public class Main {
         System.out.print("Quantas pessoas serao digitadas? ");
         int n = scanner.nextInt();
 
-        Pessoas[] vetor = new Pessoas[n];
+        String[] pessoas = new String[n];
+        int[] idades = new int[n];
+        double[] alturas = new double[n];
+
+        double soma, media, menor, porcent;
+        soma = 0.0;
+        menor = 0.0;
 
         for(int i = 0; i < n; i++){
-            System.out.printf("Digite o nome da %da pessoa: ", i);
+            System.out.printf("Dados da %da pessoa:%n", i);
 
+            System.out.print("Nome: ");
+            pessoas[i] = scanner.nextLine();
+
+            System.out.print("Idade: ");
+            idades[i] = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("Altura: ");
+            alturas[i] = scanner.nextDouble();
+            scanner.nextLine();
+
+            if(idades[i] < 16){
+                menor++;
+            }
+
+            soma += alturas[i];
         }
+
+        porcent = menor * 100.0 / n;
+
+        media = soma / n;
+
+        System.out.printf("Altura média: %.2f%n", media);
+
+        System.out.printf("Pessoas com menos de 16 anos: %.2f", porcent);
 
         scanner.close();
     }
