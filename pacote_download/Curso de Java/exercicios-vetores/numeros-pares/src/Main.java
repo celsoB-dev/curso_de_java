3fe7a7c4aaad67880d0a6ab0b127e4bde2e639d3
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-19 11:30:01 -0300

    Update Main.java

--- pacote_download/Curso de Java/exercicios-vetores/numeros-pares/src/Main.java
+++ pacote_download/Curso de Java/exercicios-vetores/numeros-pares/src/Main.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Main {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+        scanner.close();
+    }
 }
