99fdbb1e1dac167d53fb20982ede6d92f38e7221
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-19 12:11:16 -0300

    Update Main.java

--- pacote_download/Curso de Java/exercicios-vetores/maior-posicao/src/Main.java
+++ pacote_download/Curso de Java/exercicios-vetores/maior-posicao/src/Main.java
@@ -8,7 +8,29 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int n = scanner.nextInt();
+
+        double[] vetor = new double[n];
+
+        for(int i  = 0; i < n; i++){
+            System.out.print("Digite um número: ");
+            vetor[i] = scanner.nextDouble();
+        }
+
+        double maiorNumero = 0;
+        int posicao = -1;
+
+        for(int i = 0; i < n; i++){
+            if(vetor[i] > maiorNumero){
+                maiorNumero = vetor[i];
+                posicao++;
+            }
+        }
+
+        System.out.println("\nMAIOR VALOR = " + maiorNumero);
+        System.out.println("POSIÇÃO DO MAIOR VALOR = " + posicao);
 
         scanner.close();
     }
