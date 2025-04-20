7f4c426ca4e949fa23b85c04fe8b25f3b049c044
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-20 13:18:55 -0300

    Update Program.java

--- pacote_download/Curso de Java/exercicios-vetores/abaixo-da-media/src/application/Program.java
+++ pacote_download/Curso de Java/exercicios-vetores/abaixo-da-media/src/application/Program.java
@@ -1,7 +1,17 @@
 package application;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args){
+
+        Locale.setDefault(Locale);
+
+        Scanner scanner = new Scanner(System.in);
+
         
+
+        scanner.close();
     }
 }
