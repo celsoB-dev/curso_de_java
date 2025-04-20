c8cd906770b9ae92c82e5a3edbb05b15008248c8
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-20 13:51:07 -0300

    Update Program.java

--- pacote_download/Curso de Java/exercicios-vetores/media-pares/src/Program.java
+++ pacote_download/Curso de Java/exercicios-vetores/media-pares/src/Program.java
@@ -8,7 +8,32 @@ public class Program {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        int n = scanner.nextInt();
+
+        int[] numeros = new int[n];
+
+        double somaPares = 0.0;
+        double numerosPares = 0.0;
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.print("Digite um numero: ");
+            numeros[i] = scanner.nextInt();
+
+            if(numeros[i] % 2 == 0){
+                somaPares += numeros[i];
+                numerosPares++;
+            }
+        }
+
+        if(numerosPares > 0){
+            double media = somaPares / numerosPares;
+            System.out.printf("%nMEDIA DOS PARES = %.1f%n", media);
+        }
+
+        else{
+            System.out.printf("%nNENHUM NUMERO PAR%n");
+        }
 
         scanner.close();
     }
