1b3f916037fae3e2503abf089becc2a905ec11e2
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-20 21:58:15 -0300

    Update Program.java

--- pacote_download/Curso de Java/exercicios-vetores/mais-velho/src/Program.java
+++ pacote_download/Curso de Java/exercicios-vetores/mais-velho/src/Program.java
@@ -13,7 +13,32 @@ public class Program {
 
         String[] nomes = new String[n];
         int[] idade = new int[n];
+        scanner.nextLine();
 
+        for(int i = 0; i < n; i++){
+            System.out.printf("Dados da %d pessoa:%n", i + 1);
+
+            System.out.print("Nome: ");
+            nomes[i] = scanner.nextLine();
+
+            System.out.print("Idade: ");
+            idade[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        int maisVelho = idade[0];
+
+        for(int i = 0; i < n; i++){
+            if(idade[i] > maisVelho){
+                maisVelho = idade[i];
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(idade[i] == maisVelho){
+                System.out.println("PESSOA MAIS VELHA: " + nomes[i]);
+            }
+        }
 
         scanner.close();
     }
