04b68e2510284cb5fa105679a0664ad03771644c
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-04-20 22:15:13 -0300

    Update Program.java

--- pacote_download/Curso de Java/exercicios-vetores/aprovados/src/Program.java
+++ pacote_download/Curso de Java/exercicios-vetores/aprovados/src/Program.java
@@ -8,23 +8,34 @@ public class Program {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Quantos alunos serão digitados?");
+        System.out.print("Quantos alunos serão digitados? ");
         int n = scanner.nextInt();
         scanner.nextLine();
 
         String[] nome = new String[n];
         double[] nota1 = new double[n];
         double[] nota2 = new double[n];
+        double[] media = new double[n];
 
         for(int i = 0; i < n; i++){
-            System.out.printf("Digite nome, primeira e segunda nota do %d aluno:%n", i);
+            System.out.printf("Digite nome, primeira e segunda nota do %d aluno:%n", i+1);
             nome[i] = scanner.nextLine();
-            
+
             nota1[i] = scanner.nextDouble();
             scanner.nextLine();
 
             nota2[i] = scanner.nextDouble();
             scanner.nextLine();
+
+            media[i] = nota1[i] + nota2[i] / n;
+        }
+
+        System.out.println("Alunos aprovados:");
+
+        for(int i = 0; i < n; i++){
+            if(media[i] >= 6.0){
+                System.out.println(nome[i]);
+            }
         }
 
         scanner.close();
