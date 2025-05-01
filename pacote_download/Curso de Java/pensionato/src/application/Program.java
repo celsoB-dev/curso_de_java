450ea34ed57db7f217aac9a26331b439edaa5cbd
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-05-01 19:13:21 -0300

    Pensionato

--- pacote_download/Curso de Java/pensionato/src/application/Program.java
+++ pacote_download/Curso de Java/pensionato/src/application/Program.java
@@ -1,9 +1,43 @@
 package application;
 
 import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
 
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many rooms will be rented? ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        Product[] vect = new Product[n];
+
+        for(int i = 0; i < vect.length; i++){
+            vect[i] = new Product();
+            System.out.printf("Rent #%d%n", i+1);
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("Email: ");
+            String email = scanner.nextLine();
+            System.out.print("Room ");
+            int room = scanner.nextInt();
+            scanner.nextLine();
+            vect[i] = new Product(name, email, room);
+        }
+
+        System.out.println("Busy rooms: \n");
+
+        for(int i = 0; i < vect.length; i++){
+            if(vect[i] != null){
+                System.out.printf("%d: %s, %s%n", vect[i].getRoom(), vect[i].getName(), vect[i].getEmail());
+            }
+        }
+
+        scanner.close();
     }
 }
--- pacote_download/Curso de Java/pensionato/src/entities/Product.java
+++ pacote_download/Curso de Java/pensionato/src/entities/Product.java
@@ -1,4 +1,41 @@
 package entities;
 
 public class Product {
+
+    private String name;
+    private String email;
+    private int room;
+
+    public Product(String name, String email, int room) {
+        this.name = name;
+        this.email = email;
+        this.room = room;
+    }
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
 }
