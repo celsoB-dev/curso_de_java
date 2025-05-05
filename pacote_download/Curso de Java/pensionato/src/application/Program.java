a96093fd4d4c6b0973185ca327f7e1af52197cb2
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-05-04 22:14:33 -0300

    Update Program.java

--- pacote_download/Curso de Java/pensionato/src/application/Program.java
+++ pacote_download/Curso de Java/pensionato/src/application/Program.java
@@ -15,26 +15,27 @@ public class Program {
         int n = scanner.nextInt();
         scanner.nextLine();
 
-        Product[] vect = new Product[n];
+        Product[] rooms = new Product[10];
 
-        for (int i = 0; i < vect.length; i++) {
-            vect[i] = new Product();
-            System.out.printf("Rent #%d%n", i + 1);
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("Rent #%d%n", i);
             System.out.print("Name: ");
             String name = scanner.nextLine();
             System.out.print("Email: ");
-            String email = scanner.nextLine();
-            System.out.print("Room ");
-            int room = scanner.nextInt();
+            String email = scanner.next();
+            System.out.print("Room: ");
+            int num = scanner.nextInt();
             scanner.nextLine();
-            vect[i] = vect[room];
-            vect[i] = new Product(name, email);
+            Product rent = new Product(name, email);
 
-
+            rooms[num] = rent;
         }
-        for (int i = 0; i < vect.length; i++) {
-            if (vect[i] != null) {
-                System.out.printf("%d%s%n", i, vect[i]);
+
+        System.out.println("\nBusy rooms: ");
+
+        for (int i = 0; i < 10; i++) {
+            if (rooms[i] != null) {
+                System.out.printf("%d: %s%n", i, rooms[i]);
             }
         }
         scanner.close();
