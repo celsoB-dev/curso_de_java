190c9b37514e948f4ba690ac029781d5ecb89e98
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-06-01 19:31:52 -0300

    Update Main.java

--- pacote_download/Curso de Java/laco-for-each/src/Main.java
+++ pacote_download/Curso de Java/laco-for-each/src/Main.java
@@ -8,7 +8,11 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+        for (String obj : vect){
+            System.out.println(obj);
+        }
 
         scanner.close();
     }
